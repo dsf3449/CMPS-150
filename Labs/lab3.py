15d09dad0209b468b644fe134dfd5b6c8a6da21c
dsf3449 <dsf3449@louisiana.edu>
2017-01-31 12:32:31 -0600

    Began work on lab 3

---
+++ Labs/lab3.py
@@ -0,0 +1,51 @@
+# lab3.py
+
+# Author:       David Fontenot
+# CLID/Section: dsf3449/C00177513/Section 002
+ 
+ 
+# print a couple of blank lines before the program begins
+print()
+print()
+# asking for input
+
+
+
+# ask the user for the radius of the pizza (in inches)
+radius = eval(input("Enter radius of pizza:"))
+# compute the area of the pizza (in square inches) 
+pizzaArea = 
+
+# ask the user for the price of the pizza
+price = eval(input("Enter price of pizza"))
+# compute the price of the pizza per square inch 
+
+
+
+# print out the price of the pizza per square inch 
+
+
+
+
+
+
+# print a couple of blank lines before the next section of code begins
+# asking for input 
+
+
+
+# ask the user to enter a 3-letter word
+# you are allowed only ONE input statement
+
+word = input("Enter a 3-letter word: ")
+
+# print the ASCII code (a number) for each letter of the word just input
+
+
+
+
+
+
+# print a couple of blank lines when the program is done
+print
+
