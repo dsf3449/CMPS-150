df948be26203a37b8eebc756a7c0c173fa408d0b
dsf3449 <dsf3449@louisiana.edu>
2017-02-08 11:17:36 -0600

    Added lab4 & mpl chapter 3

---
+++ MyProgrammingLab/chapter 3.py
@@ -0,0 +1,121 @@
+# Chapter 2
+
+# Section 2.5
+# 51014
+precise = 1.09388641
+
+# 51019
+eight = 8
+
+# 51013
+temperature = 98.6
+
+# 51043
+length = 3.5
+width = 1.55
+
+# Section 2.6
+# 51168
+jtemp = j
+itemp = i
+j = itemp
+i = jtemp
+
+# 51169
+jtemp = j
+itemp = i
+j = itemp
+i = jtemp
+
+# 51170
+fpwtemp = first_place_winner
+spwtemp = second_place_winner
+second_place_winner = fpwtemp
+first_place_winner = spwtemp
+
+# 51171
+bvtemp = best_value
+sbvtemp = second_best_value
+best_value = sbvtemp
+second_best_value = bvtemp
+
+# 51093
+grad_age = 4 + matric_age
+
+# Section 2.8
+# 51872
+small + large
+
+# 51029
+verbal_score + math_score
+
+# 51030
+taxable_purchases + tax_free_purchases
+
+# 51031
+ending_time - starting_time
+
+# 51032
+full_admission_price - discount_amount
+
+# 51033
+price_per_case * 12
+
+# 51034
+cost_of_bus_rental / max_bus_riders
+
+# 51035
+principal % divisor
+
+# 51044
+total1 + total2
+
+# 51045
+sales_summer - sales_spring
+
+# 51046
+total_weight - weight_of_box
+
+# 51048
+price * total_number
+
+# 51047
+total_weight / quantity
+
+# 51249
+g = h * h
+
+# 51880
+price % 100
+
+# 51881
+x % 10
+
+# Section 2.9
+# 51037
+(exam1 + exam2) / 2
+
+# 51020
+avg = (a + b + c) / 3
+
+# 51023
+bmi = (w / (h * h)) * 703
+
+# 51871
+(width1 * length1) + (width2 * length2)
+
+# 51873
+(small + large) ** 2
+
+# 51875
+1 + (1/2) + (1/3) + (1/4) + (1/5) + (1/6) + (1/7) + (1/8)
+
+# Section 2.10
+# 51094
+bridge_players = bridge_players + 4
+
+# 51095
+profits = profits * 10
+
+# 51189
+total = amount + total
