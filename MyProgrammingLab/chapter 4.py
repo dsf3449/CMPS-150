decd4aaa751778fd7ab5433096b76b5b7ea3d773
dsf3449 <dsf3449@louisiana.edu>
2017-02-08 22:51:00 -0600

    Testing new features

---
+++ MyProgrammingLab/chapter 4.py
@@ -0,0 +1,38 @@
+# Chapter 3
+
+# 51833
+"!!!!!"
+
+# 51835
+""
+
+# 51923
+empty = ""
+
+# 51886
+background = "white"
+selectionColor = "blue"
+
+# 51887
+foreground = "red"
+
+# 51888
+text = ""
+
+# 51916
+oneSpace = " "
+
+# 51754
+"Hello" + "World"
+
+# 51756
+name1+","+name2+","+name3
+
+# 51838
+"http://"+address
+
+# 51839
+prefix + suffix
+
+# 51869
+"("+word+")"
