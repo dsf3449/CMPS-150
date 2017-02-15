8a5f24cbb2e244687c8f85519ef5e7016c699eae
dsf3449 <dsf3449@louisiana.edu>
2017-02-15 12:15:57 -0600

    Finished lab 5

--- Labs/lab5.py
+++ Labs/lab5.py
@@ -4,10 +4,21 @@
 
 
 # ask the user to enter an x,y coordinate
-coords = eval(input("Enter a coordinate pair (separated by a comma):"))
+coords = eval(input("Enter a coordinate pair (separated by a comma): "))
 
 
 # check for the origin, x-axis OR y-axis & all 4 quadrants
-if coords == (0,0):
+if coords[0] == 0 and coords[1] == 0:
     print("This point is the origin.")
-elif coords == (0
+elif coords[0] == 0 or coords[1] == 0:
+    print("This point is on an axis.")
+elif coords[0] > 0 and coords[1] > 0:
+    print("This point is in Quadrant I.")
+elif coords[0] < 0 and coords[1] > 0:
+    print("This point is in Quadrant II.")
+elif coords[0] < 0 and coords[1] < 0:
+    print("This point is in Quadrant III.")
+elif coords[0] > 0 and coords[1] < 0:
+    print("This point is in Quadrant IV.")
+else:
+    print("Invalid input.")
