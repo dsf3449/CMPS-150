bbdc3469fca571f77f03472ac4683765beb0ce94
dsf3449 <dsf3449@louisiana.edu>
2017-02-17 11:38:32 -0600

    Started chapter 4 MPL

---
+++ MyProgrammingLab/chapter 4.py
@@ -0,0 +1,54 @@
+# Section 4.2
+# 51052
+x == 0
+
+# 51053
+profits == losses
+
+# 51054
+c != ' '
+
+# 51055
+index > last_index
+
+# 51056
+hours_worked > 40
+
+# 51057
+x >= y
+
+# 51058
+number_of_men >= number_of_women
+
+# 51059
+average < 60.5
+
+# 51060
+gross_pay < 10000
+
+# 51108
+x > y
+
+# 51893
+if hoursWorked > 40:
+    workedOvertime = True
+else:
+    workedOvertime = False
+
+# 51891
+hasPassedTest = True
+
+# 51892
+if numberOfSides == 4:
+    isQuadrilateral = True
+else:
+    isQuadrilateral = False
+
+# 51882
+c == '\n'
+
+# 51883
+c == ' '
+
+# Section 4.4
+# 
