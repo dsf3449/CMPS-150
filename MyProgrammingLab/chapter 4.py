f764e7882ab496fe79dc411617fe030216b76e6e
dsf3449 <dsf3449@louisiana.edu>
2017-02-22 23:59:36 -0600

    Finished mpl 4

--- MyProgrammingLab/chapter 4.py
+++ MyProgrammingLab/chapter 4.py
@@ -51,4 +51,106 @@ c == '\n'
 c == ' '
 
 # Section 4.4
-# 
+# 51114
+if temperature > 98.6:
+    fever = True
+
+# 51115
+if goods_sold > 500000:
+    bonus = 10000
+
+# 51116
+if outside_temperature > 90:
+    shelf_life = shelf_life - 4
+
+# 51117
+if worked_overtime == True:
+    pay = pay * 1.5
+
+# 51102
+if name1 > name2:
+    first = name1
+elif name2 > name1:
+    first = name2
+
+# 51217
+if x > y:
+    max = x
+elif y > x:
+    max = y
+
+# 51897
+if isIsosceles == True:
+    isoCount = isoCount + 1
+    triangleCount = triangleCount + 1
+    polygonCount = polygonCount + 1
+
+# Section 4.6
+# 51118
+if age >= 65:
+    senior_citizens = senior_citizens + 1
+else:
+    non_seniors = non_seniors + 1
+
+# 51119
+if sold_yesterday > sold_today:
+    sales_trend = -1
+else:
+    sales_trend = 1
+
+# 51120
+if temperature > 98.6:
+    fever = True
+else:
+    fever = False
+
+# Section 4.7
+# 51121
+if age < 18:
+    minors = minors + 1
+elif age >= 18 and age <= 64:
+    adults = adults + 1
+else:
+    seniors = seniors + 1
+
+# 51122
+if ph < 7:
+    neutral, base, acid = 0, 0, 1
+elif ph > 7:
+    neutral, base, acid = 0, 1, 0
+else:
+    neutral, base, acid = 1, 0, 0
+
+# Section 4.11
+# 51109
+worked_overtime == True
+
+# 51112
+age < 19 or is_full_time_student == True
+
+# 51113
+is_a_member == False
+
+# 51173
+on_off_switch = not on_off_switch
+
+# 51061
+x >= 0 and y < 0
+
+# 51062
+x >= 0 or y < 0
+
+# 51165
+number_of_prizes % number_of_participants == 0
+
+# 51166
+width_of_box % width_of_book != 0
+
+# 51167
+x % 2 == 0
+
+# 51110
+temperature > 90 and humidity < 10
+
+# 51111
+years_with_company < 5 and department != 99
