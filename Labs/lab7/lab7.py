6cae040195f5160c8169eee4a30b03bbe1e6b145
dsf3449 <dsf3449@louisiana.edu>
2017-03-08 11:07:31 -0600

    commented for grading..

--- Labs/lab7/lab7.py
+++ Labs/lab7/lab7.py
@@ -1,21 +1,23 @@
 # Author:       David Fontenot
 # CLID/Section: dsf3449/C00177513/Section 002
 # Lab #7
- 
+
+# Open the file
 inFile = open("lab7.fuel")
-    
+
+# Print the top of the chart
 print()
 print("    Fuel Type        Gallons     Bill")
 print("-------------------------------------")  
 
-count = 0
-
-# this is a count-controlled loop
+# We want the program to read 15, exactly.
 for i in range(0, 15):
-    # read and process data
+    # Read the data from the file
     fuelType = inFile.readline().strip()
     fuelAmount = eval(inFile.readline())
 
+    # Assign the full fuel type based on the fuelType read from the file
+    # then calculate the bill based on the cost for that fuelType.
     if fuelType == 'S':
         word = "Super Unleaded"
         bill = fuelAmount * 2.62
@@ -29,9 +31,11 @@ for i in range(0, 15):
         word = "Diesel"
         bill = fuelAmount * 2.35
 
+    # Assign fuelAmount to gallons for printing
     gallons = fuelAmount
-    # print each line of output
+    
+    # Print each line of output
     print(format(word,'20s'),format(gallons,'7.2f'),format(bill,'8.2f'))  
 
-
+# Close the file to free memory
 inFile.close()
