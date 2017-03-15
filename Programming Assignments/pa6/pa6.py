0190f4d05f037f0b45566d89cec14f3d8cfc464a
dsf3449 <dsf3449@louisiana.edu>
2017-03-14 23:25:56 -0500

    commented

--- Programming Assignments/pa6/pa6.py
+++ Programming Assignments/pa6/pa6.py
@@ -11,46 +11,59 @@
 # Certificate of Authenticity:
 # I certify that this assignment is entirely my own work.
 
+# Open the file for reading
 file = open("pa6numbers.py", 'r')
 
+# Initilize variables
 maxInf = 0
 minSen = 10000
 maxInfCity = ''
 minSenCity = ''
 
+# Print the top of the table
 print(format("City/State", '17s'), format("Pneumonia Rate", '16s'), format("Infant Rate", '15s'), "Senior Rate")
 print("--------------------------------------------------------------")
 
 while True:
     # Get the city / state, needs strip for newline char
     line = file.readline().strip()
-
+    
+    # Read all the numerical values and evaluate them
     all = eval(file.readline())
     pnu = eval(file.readline())
     inf = eval(file.readline())
     sen = eval(file.readline())
-
+    
+    # Exit the loop if we reach the sentinal value
     if all == -1:
         break
     
+    # Do math to get the percentages
     pnuRate = (pnu/all) * 100
     infRate = (inf/all) * 100
     senRate = (sen/all) * 100
+    
+    # Print the line!
     print(format(line, '20s'), format(pnuRate, '7.4f'), format(infRate, '15.4f'), format(senRate, '15.4f'))
-
+    
+    # Determine if the infant rate of the current city is greater than the previous city
     if infRate > maxInf:
+        # If so, assign that value to the max variable
         maxInf = infRate
         maxInfCity = line
 
+    # Same as above, just if the senior rate is less than the previous city
     if senRate < minSen:
         minSen = senRate
         minSenCity = line
 
+# Print the maximum infant rate
 print()
 print("Maximum Infant Rate")
 print("------------------------")
 print(format(maxInfCity, '17s'), format(maxInf, '0.4f'))
 
+# Print the minimum senior rate
 print()
 print("Minimum Senior Rate")
 print("------------------------")
