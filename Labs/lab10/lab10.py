0114876ecad659d9a27e11bab71ece6eb7483533
dsf3449 <dsf3449@louisiana.edu>
2017-03-29 22:28:30 -0500

    fuck the other method

--- Labs/lab10/lab10.py
+++ Labs/lab10/lab10.py
@@ -3,16 +3,20 @@
 
 def process(type, previousBalance, changer):
     if type == 'W':
-        newBalance = (previousBalance - changer), "Withdrawal"
-        if newBalance[0] < 0:
-            return "NEC Exception"
-        elif newBalance[0] > 0:
+        newBalance = (previousBalance - changer)
+        if newBalance < 0:
+            print(format("Withdrawal", '15s'), format(changer, '0.2f'), format("Insufficient Funds", '>23s'))
+            return previousBalance
+        elif newBalance > 0:
+            print(format("Withdrawal", '15s'), format(changer, '0.2f'), format(newBalance, '11.2f'))
             return newBalance
     elif type == 'D':
-        newBalance = (previousBalance + changer), "Deposit"
+        newBalance = (previousBalance + changer)
+        print(format("Deposit", '15s'), format(changer, '0.2f'), format(newBalance, '11.2f'))
         return newBalance
     elif type == 'B':
-        newBalance = previousBalance, "Balance"
+        newBalance = previousBalance
+        print(format("Balance", '26s'), format(newBalance, '0.2f'))
         return newBalance
     elif type == 'X':
         print()
@@ -40,8 +44,6 @@ def main():
     while (trans != 'X'):
         # process data just read from the file
         balance = process(trans, balance, amount)
-        print(balance[1], balance[0])
-        balance = balance[0]
         # get next set of data from the file
         trans = infile.readline().strip()
         amount = eval(infile.readline())
