6ec86d04d147d9a66de042170a05dfcee06d9ed8
dsf3449 <dsf3449@louisiana.edu>
2017-04-03 11:48:04 -0500

    Started MPL 6

---
+++ MyProgrammingLab/chapter 6.py
@@ -0,0 +1,34 @@
+# Section 6.2
+# 51016
+def typing_speed(words, time):
+    minutes = time / 60
+    return words / minutes
+
+# 51155
+def twice(int):
+    return int * 2
+
+# 51156
+def add(number1, number2):
+    return number1 + number2
+
+# 51158
+def power_to(first, second):
+    if second < 0:
+	return 0
+    else:
+	return first ** second
+
+# Section 6.3
+# 51143
+print_todays_date()
+
+# 51144
+print_error_description(14)
+
+# 51146
+eurasia_sales = add(euro_sales, asia_sales)
+
+# 51147
+cube_volume = to_the_power_of(cube_side, 3)
+
